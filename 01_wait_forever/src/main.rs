65e0eb2611d3aba46bc1bb53afc63550c36e699e
Li Shaowen <li-shaowen879@g.ecc.u-tokyo.ac.jp>
2023-04-08 16:46:00 +0900

    print

--- 01_wait_forever/src/main.rs
+++ 01_wait_forever/src/main.rs
@@ -3,13 +3,20 @@
 // Copyright (c) 2018-2022 Andre Richter <andre.o.richter@gmail.com>
 
 #![feature(asm_const)]
+#![feature(format_args_nl)]
+#![feature(panic_info_message)]
+#![feature(custom_test_frameworks)]
 #![no_main]
 #![no_std]
 
 mod bsp;
+mod console;
 mod cpu;
 mod panic_wait;
+mod print;
 
 unsafe fn kernel_init() -> ! {
-    panic!()
+    println!("Hello from Pi!");
+
+    panic!("Stopping here.")
 }
--- 01_wait_forever/src/panic_wait.rs
+++ 01_wait_forever/src/panic_wait.rs
@@ -4,14 +4,38 @@
 
 //! A panic handler that infinitely waits.
 
-use crate::cpu;
+use crate::{cpu, println};
 use core::panic::PanicInfo;
 
-//--------------------------------------------------------------------------------------------------
-// Private Code
-//--------------------------------------------------------------------------------------------------
+fn panic_prevent_reenter() {
+    use core::sync::atomic::{AtomicBool, Ordering};
+
+    static PANIC_IN_PROGRESS: AtomicBool = AtomicBool::new(false);
+
+    if !PANIC_IN_PROGRESS.load(Ordering::Relaxed) {
+        PANIC_IN_PROGRESS.store(true, Ordering::Relaxed);
+        return;
+    }
+
+    cpu::wait_forever()
+}
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    panic_prevent_reenter();
+
+    let (location, line, col) = match info.location() {
+        Some(loc) => (loc.file(), loc.line(), loc.column()),
+        _ => ("?", 0, 0),
+    };
+
+    println!(
+        "Kernel Panic!\n\nPanic localtion:\n  File '{}', line {}, column {}\n\n {}",
+        location,
+        line,
+        col,
+        info.message().unwrap_or(&format_args!(""))
+    );
+
     cpu::wait_forever()
 }
